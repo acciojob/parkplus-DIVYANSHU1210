3c8f2d169c1ed382c7cbba8d7cc84e86021df84a
Divyanshu Joshi <divyanshujoshi1012@gmail.com>
2023-06-19 23:03:49 +0530

    7th

--- src/main/java/com/driver/model/Payment.java
+++ src/main/java/com/driver/model/Payment.java
@@ -47,7 +47,7 @@ public class Payment {
         this.id = id;
     }
 
-    public Boolean getPaymentCompleted() {
+    public Boolean isPaymentCompleted() {
         return paymentCompleted;
     }
 
